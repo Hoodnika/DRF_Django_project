b3ffd7462908c3c6d819f731b99e23463a06f7ab
nikita <hoodnika@gmail.com>
2024-07-08 03:12:58 +0500

    courseapp

---
+++ courseapp/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+
+from config.special_elements import NULLABLE
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=300, verbose_name='название')
+    preview = models.ImageField(**NULLABLE, upload_to='courses/preview', verbose_name='превью (картинка)')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=300, verbose_name='название')
+    preview = models.ImageField(**NULLABLE, upload_to='lessons/preview', verbose_name='превью (картинка)')
+    description = models.TextField(verbose_name='описание')
+    url = models.TextField(verbose_name='ссылка на видео')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс')
+
+    def __str__(self):
+        return f'{self.title} ({self.course})'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
---
+++ courseapp/serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+
+from courseapp.models import Course, Lesson
+
+
+class CourseSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+
+class LessonSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Lesson
+        fields = '__all__'
