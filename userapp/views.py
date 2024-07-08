59eff5737d7f8171ff6db0de32c2f914dc850933
nikita <hoodnika@gmail.com>
2024-07-09 03:12:28 +0500

    add serializers and functionality for them

--- userapp/views.py
+++ userapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import generics
+from rest_framework.filters import OrderingFilter
 
-# Create your views here.
+from userapp.models import Payment
+from userapp.serializer import PaymentSerializer
+
+
+class PaymentListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ('paid_course', 'paid_lesson', 'payment_type')
+    ordering_fields = ('datetime_payment',)
+
+
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = PaymentSerializer
+    queryset = Payment.objects.all()
