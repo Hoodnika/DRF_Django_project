3d7b4b7897929a24863b6759218dfb241d406aa2
nikita <hoodnika@gmail.com>
2024-07-09 03:06:24 +0500

    new path to userapp

--- config/urls.py
+++ config/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('courseapp.urls', namespace='courseapp')),
+    path('', include('userapp.urls', namespace='userapp')),
 ]
