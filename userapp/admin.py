8a1ea62f6a30560a72c76e5042d140e846c548bd
nikita <hoodnika@gmail.com>
2024-07-10 01:58:36 +0500

    admin_panel

--- userapp/admin.py
+++ userapp/admin.py
@@ -5,11 +5,11 @@ from userapp.models import User, Payment
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name', 'last_name', 'is_superuser', 'is_staff')
+    list_display = ('email', 'first_name', 'last_name', 'is_superuser', 'is_staff', 'id')
     list_filter = ('email', 'is_superuser')
 
 
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    list_display = ('user', 'datetime_payment', 'cost', 'paid_course', 'paid_lesson', 'payment_type')
-    list_filter = ('cost', 'user',)
+    list_display = ('owner', 'datetime_payment', 'cost', 'paid_course', 'paid_lesson', 'payment_type', 'id')
+    list_filter = ('cost', 'owner',)
