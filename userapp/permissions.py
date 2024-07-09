b4908532936f199b35a793d543fdfdfb2ad117b0
nikita <hoodnika@gmail.com>
2024-07-10 01:59:06 +0500

    new file with permissions

---
+++ userapp/permissions.py
@@ -0,0 +1,23 @@
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission
+
+
+class IsModer(BasePermission):
+    message = 'Ошибка в правах доступа'
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name='moder').exists()
+
+
+class IsOwner(BasePermission):
+    message = 'Adding customers not allowed'
+
+    def has_object_permission(self, request, view, obj):
+        return obj.owner == request.user
+
+
+class IsMe(BasePermission):
+    message = 'Это не твой профиль, ты куда лезешь'
+
+    def has_object_permission(self, request, view, obj):
+        return obj == request.user
