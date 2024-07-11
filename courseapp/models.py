f8708b8f3b948efa8f42e148e94d7163c1780b3d
nikita <hoodnika@gmail.com>
2024-07-12 02:55:08 +0500

    add model Subscription

--- courseapp/models.py
+++ courseapp/models.py
@@ -10,7 +10,6 @@ class Course(models.Model):
     description = models.TextField(verbose_name='описание')
     owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.SET_NULL, verbose_name='владелец', **NULLABLE)
 
-
     def __str__(self):
         return f'{self.title}'
 
@@ -33,3 +32,8 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Subscription(models.Model):
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='пользователь', related_name='subscription')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name='курс', related_name='subscription')
