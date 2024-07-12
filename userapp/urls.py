a993f052d909a2a6056bc6e171373955d77501bb
nikita <hoodnika@gmail.com>
2024-07-13 04:17:56 +0500

    new urls for new functionality

--- userapp/urls.py
+++ userapp/urls.py
@@ -3,8 +3,8 @@ from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from userapp.apps import UserappConfig
-from userapp.views import PaymentListAPIView, UserPaymentListAPIView, UserCreateAPIView, UserRetrieveAPIView, \
-    PaymentCreateAPIView, UserUpdateAPIView, UserDestroyAPIView
+from userapp.views import PaymentListAPIView, UserListAPIView, UserCreateAPIView, UserRetrieveAPIView, \
+    PaymentCreateAPIView, UserUpdateAPIView, UserDestroyAPIView, payment_success
 
 app_name = UserappConfig.name
 
@@ -13,12 +13,13 @@ urlpatterns = [
     path('token/refresh', TokenRefreshView.as_view(permission_classes=(AllowAny,)), name='token_refresh'),
 
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    path('user/', UserPaymentListAPIView.as_view(), name='user_list'),
+    path('user/', UserListAPIView.as_view(), name='user_list'),
     path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user_get'),
     path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
     path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user_delete'),
 
     path('payment/', PaymentListAPIView.as_view(), name='payment_list'),
-    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment_create')
+    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment_create'),
+    path('payment_success/', payment_success, name='payment_success')
 
 ]
