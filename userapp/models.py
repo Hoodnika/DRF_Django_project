e3e40e9998b707ce03f0fcce25c291ac1f8e81e0
nikita <hoodnika@gmail.com>
2024-07-13 04:15:28 +0500

    edit model Payment

--- userapp/models.py
+++ userapp/models.py
@@ -30,21 +30,15 @@ class Payment(models.Model):
         cash = 'Наличные'
         transfer = 'Переводом'
 
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='пользователь', related_name='payment',
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, verbose_name='пользователь', related_name='payment',
                               **NULLABLE)
     datetime_payment = models.DateTimeField(verbose_name='дата оплаты', auto_now_add=True)
-    cost = models.PositiveIntegerField(verbose_name='сумма оплаты')
-
-    # class Paid_material(models.Choices):
-    #     Course = Course
-    #     Lesson = Lesson
-    #
-    # paid_material = models.ForeignKey(choices=Paid_material.choices, on_delete=models.CASCADE, verbose_name='оплаченный материал')
-
+    price = models.PositiveIntegerField(verbose_name='сумма оплаты', **NULLABLE)
     paid_course = models.ForeignKey(Course, on_delete=models.SET_NULL, **NULLABLE, related_name='payment')
     paid_lesson = models.ForeignKey(Lesson, on_delete=models.SET_NULL, **NULLABLE, related_name='payment')
     payment_type = models.CharField(choices=Type_payment.choices, max_length=16, verbose_name='способ оплаты')
-
+    session_id = models.CharField(max_length=300, verbose_name=' id сессии', **NULLABLE)
+    link = models.URLField(max_length=400, verbose_name='Ссылка на оплату', **NULLABLE)
     class Meta:
         verbose_name = 'Оплата'
         verbose_name_plural = 'Оплаты'
